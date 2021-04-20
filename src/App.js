686e05f8c6ef56607ce3667050cee3148516b765
David Cid <david.cid@gmail.com>
2021-04-20 14:02:08 +0200

    header section styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Router>
-          <>
-            <Switch>
-              <Route path="/" exact>
-                <h1>This is the homepage</h1>
-              </Route>
-            </Switch>
-          </>
-        </Router>
+        <>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
