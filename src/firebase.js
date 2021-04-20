8147b9262ef61ba12839bb14c74e001bf9fe0a2b
David Cid <david.cid@gmail.com>
2021-04-20 11:59:08 +0200

    first configs

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCu2CSKx400IS2gO2IwRsW455fd_qvsHY",
+    authDomain: "slack-clone-864c6.firebaseapp.com",
+    projectId: "slack-clone-864c6",
+    storageBucket: "slack-clone-864c6.appspot.com",
+    messagingSenderId: "9985896018",
+    appId: "1:9985896018:web:51468a6706d78c4761398b"
+  };
